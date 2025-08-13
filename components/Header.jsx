e0982695871538070d4f157d3ae4a0e49e4e1ffe
Protsahan Lama <protsahanlama123@gmail.com>
2025-08-13 17:08:34 -0600

    Add files via upload
    Added components for navbar, header and footer as well as external pages

---
+++ components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="bg-green-800 text-center p-6">
+      <h1 className="text-4xl font-bold text-white">SAIT Zoo Inventory</h1>
+      <p className="mt-2 text-md text-white">Find out what animals live in our zoo!</p>
+    </header>
+  )
+}
